3dab55ccc5c3a7dfba8ce38ec2d2fbce29820d6d
Isha Paghdal <ishapaghdal@gmail.com>
2023-10-14 00:20:20 +0530

    Produt CRUD Operation Added

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,6 +10,7 @@ namespace BookWeb.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
 
         void Save();
     }
---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Book.DataAccess.Data;
+using Book.DataAccess.Repository.IRepository;
+using Book.Models;
+using Book.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product> ,IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)  
+        {
+            _db = db;
+        }
+        
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
